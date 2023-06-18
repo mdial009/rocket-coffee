cf7a940506f3b41eca9cf82778b3379dce3efcba
Madany Diallo <Madanyx@gmail.com>
2023-06-18 18:20:46 -0400

    updates

--- src/components/ProductListItem/ProductListItem.js
+++ src/components/ProductListItem/ProductListItem.js
@@ -6,16 +6,41 @@ export default function ProductListItem({
   price,
   imageUrl,
   onAddToCart,
+  isSoldOut,
+  isOnSale,
 }) {
   return (
-    <div className="card">
-      <h2>{name}</h2>
+    
+    <div className={isOnSale ? "on-sale" : "card"}>
+      <h2>
+        {name}
+        {isOnSale ? " (On Sale)" : ""}
+      </h2>
 
-      <img src={imageUrl} alt="Image" />
+      <img src={imageUrl} alt="" />
 
       <small>{price}</small>
 
-      <button onClick={onAddToCart}>Add to Cart</button>
+      <button onClick={onAddToCart} disabled={isSoldOut}>
+        {isSoldOut ? "Sold out" : "Add to Cart"}
+      </button>
     </div>
   );
+}
+
+function Heading({ children }) {
+  return <h2>{children}</h2>;
+}
+
+function Card({ children, highlight }) {
+  const cardClassName = highlight ? "card sale" : "card";
+  return <div className={cardClassName}>{children}</div>;
+}
+
+function Text({ children }) {
+  return <span>{children}</span>;
+}
+
+function Button({ onClick, children }) {
+  return <button onClick={onClick}>{children}</button>;
 }
--- src/components/ProductListItem/ProductListItem.stories.js
+++ src/components/ProductListItem/ProductListItem.stories.js
@@ -1,14 +1,44 @@
 import React from "react";
 import ProductListItem from "./ProductListItem";
 import { action } from "@storybook/addon-actions";
+import { text, withKnobs } from "@storybook/addon-knobs";
 
-export default { title: "ProductListItem" };
+export default { title: "ProductListItem", decorators: [withKnobs] };
 
-export const standard = () => (
+export const Standard = () => (
   <ProductListItem
-    name="Standard Coffee"
-    price="2.50"
+    name={text("Name", "Standard Coffee")}
+    price={text("price", "2.50")}
     onAddToCart={action("Add to cart clicked")}
-    imageUrl="https://source.unsplash.com/tNALoIZhqVM/200x100/"
+    imageUrl={text(
+      "imageUrl",
+      "https://source.unsplash.com/tNALoIZhqVM/200x100/"
+    )}
+  />
+);
+
+export const soldOut = () => (
+  <ProductListItem
+    name={text("Name", "Standard Coffee")}
+    price={text("price", "2.50")}
+    onAddToCart={action("Add to cart clicked")}
+    imageUrl={text(
+      "imageUrl",
+      "https://source.unsplash.com/tNALoIZhqVM/200x100/"
+    )}
+    isSoldOut
+  />
+);
+
+export const onSale = () => (
+  <ProductListItem
+    name={text("Name", "Standard Coffee")}
+    price={text("price", "2.50")}
+    onAddToCart={action("Add to cart clicked")}
+    imageUrl={text(
+      "imageUrl",
+      "https://source.unsplash.com/tNALoIZhqVM/200x100/"
+    )}
+    isOnSale
   />
 );
